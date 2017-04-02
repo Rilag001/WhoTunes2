66b1355182e33152e75232a3a11790bc9472d289
Ricky Lagerkvist <rickylagerkvist@harbourfront.se>
2017-04-02 23:54:13 +0200

    first commit

---
+++ app/src/main/java/se/rickylagerkvist/whotune/LogInActivity.java
@@ -0,0 +1,15 @@
+package se.rickylagerkvist.whotune;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import se.rickylagerkvist.whotune.R;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
